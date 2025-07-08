d03c798b91f5cb7ab93e33616ca7c0de59b1b7c1
LabhanshiBhatia <labhanshibhatia252006@gmail.com>
2025-07-08 14:24:59 +0530

    updates
    updated

--- home assets/navbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menu-toggle");
-  const navLinks = document.getElementById("nav-links");
-
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-});
-// This script toggles the visibility of the navigation links when the menu button is clicked.
